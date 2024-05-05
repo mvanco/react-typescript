81bc592d342b20c1fbd94f4b28b1673c66300fe2
Matus Vanco <matus.vanco@24i.com>
2024-05-05 11:48:03 +0200

    Test as library

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const URLConfig = "www.matoosh.eu";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
